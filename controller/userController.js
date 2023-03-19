79a6316b779e51a0995f6220ecff9a36df58ff32
PhatPhamCoder <phamhoangminhphat@gmail.com>
2023-03-19 23:24:01 +0700

    update signup

---
+++ controller/userController.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const path = require("path");
+const User = require("../model/user");
+const router = express.Router();
+const { upload } = require("../multer");
+const ErrorHandler = require("../utils/ErrorHandler");
+const fs = require("fs");
+const jwt = require("jsonwebtoken");
+
+router.post("/create-user", upload.single("file"), async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+    const userEmail = await User.findOne({ email });
+
+    if (userEmail) {
+      const filename = req.file.filename;
+      const filePath = `uploads/${filename}`;
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({
+            message: "Error deleting file",
+          });
+        } else {
+          res.json({
+            message: "File deleted successfully",
+          });
+        }
+      });
+      return next(new ErrorHandler("User already exists", 400));
+    }
+
+    const filename = req.file.filename;
+    const fileUrl = path.join(filename);
+
+    const user = {
+      name: name,
+      email: email,
+      password: password,
+      avatar: fileUrl,
+    };
+
+    const activationToken = createActivationToken(user);
+
+    const REACT_URL = process.env.REACT_URL;
+    const activationUrl = `${REACT_URL}/activation/${activationToken}`;
+
+    try {
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  } catch (error) {
+    return next(new ErrorHandler("User already exists", 400));
+  }
+});
+
+// Create activetion Token
+const createActivationToken = (user) => {
+  return jwt.sign(user, process.env.ACTIVATION_SECRET, {
+    expiresIn: "5m",
+  });
+};
+
+// Active User
+
+module.exports = router;
