cfd329f7b505bf3fc00a627aeaaffb27f95b4347
PhatPhamCoder <phamhoangminhphat@gmail.com>
2023-03-20 17:04:47 +0700

    finish signup

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const ErrorHandler = require("./utils/ErrorHandler");
+const ErrorHandler = require("./middleware/error");
 const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
@@ -9,13 +9,20 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 app.use("/", express.static("uploads"));
-app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+    limit: "50mb",
+  }),
+);
+
 // config
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({
     path: ".env",
   });
 }
+
 // Import routes
 const user = require("./controller/userController");
 app.use("/api/v2/user", user);
--- controller/userController.js
+++ controller/userController.js
@@ -4,8 +4,11 @@ const User = require("../model/user");
 const router = express.Router();
 const { upload } = require("../multer");
 const ErrorHandler = require("../utils/ErrorHandler");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const fs = require("fs");
 const jwt = require("jsonwebtoken");
+const sendMail = require("../utils/sendMail");
+const sendToken = require("../utils/jwtToken");
 
 router.post("/create-user", upload.single("file"), async (req, res, next) => {
   try {
@@ -20,10 +23,6 @@ router.post("/create-user", upload.single("file"), async (req, res, next) => {
           console.log(err);
           res.status(500).json({
             message: "Error deleting file",
-          });
-        } else {
-          res.json({
-            message: "File deleted successfully",
           });
         }
       });
@@ -41,11 +40,18 @@ router.post("/create-user", upload.single("file"), async (req, res, next) => {
     };
 
     const activationToken = createActivationToken(user);
-
-    const REACT_URL = process.env.REACT_URL;
-    const activationUrl = `${REACT_URL}/activation/${activationToken}`;
+    const activationUrl = `http://localhost:3000/activation/${activationToken}`;
 
     try {
+      await sendMail({
+        email: user.email,
+        subject: "Activate your account!",
+        message: `Hello ${user.name}! please click on the link to activate your account: ${activationUrl}`,
+      });
+      res.status(201).json({
+        success: true,
+        message: `please check your email: ${user.email} to activate your account!`,
+      });
     } catch (error) {
       return next(new ErrorHandler(error.message, 500));
     }
@@ -61,6 +67,42 @@ const createActivationToken = (user) => {
   });
 };
 
-// Active User
+// Activate User
+router.post(
+  "/activation",
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const { activation_token } = req.body;
+
+      const newUser = jwt.verify(
+        activation_token,
+        process.env.ACTIVATION_SECRET,
+      );
+
+      if (!newUser) {
+        return next(new ErrorHandler("Invalid token", 400));
+      }
+
+      const { name, email, password, avatar } = newUser;
+
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return next(new ErrorHandler("User already exists"), 400);
+      }
+
+      user = await User.create({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      sendToken(user, 201, res);
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }),
+);
 
 module.exports = router;
